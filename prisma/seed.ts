2e31d7ce92193b47a341f0ce2245e4bf624d87b8
Rémy Canal <remy.canal@epitech.eu>
2025-07-12 21:09:44 +0200

    [add] prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,33 +1,33 @@
-import { PrismaClient } from '@prisma/client';
-import doorsData from '../data/doors.json';
+import { PrismaClient } from "@prisma/client";
+import doorsData from "../data/doors.json";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  for (const door of doorsData.doors) {
-    await prisma.door.upsert({
-      where: { week: door.week },
-      update: {
-        opened: door.opened,
-        title: door.title,
-        text: door.text,
-      },
-      create: {
-        week: door.week,
-        opened: door.opened,
-        title: door.title,
-        text: door.text,
-      },
-    });
-  }
-  console.log('✅ 52 doors imported!');
+	for (const door of doorsData.doors) {
+		await prisma.door.upsert({
+			where: { week: door.week },
+			update: {
+				opened: door.opened,
+				title: door.title,
+				text: door.text,
+			},
+			create: {
+				week: door.week,
+				opened: door.opened,
+				title: door.title,
+				text: door.text,
+			},
+		});
+	}
+	console.log("✅ 52 doors imported!");
 }
 
 main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
