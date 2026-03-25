d59db3414667a712ba5de0753d95f89852fa2ec4
Rémy Canal <remy.canal@epitech.eu>
2026-03-25 03:04:13 +0000

    [fi] selfiiie

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,21 @@
 <template>
 	<div class="w-full text-center px-4">
+		<div class="my-20 md:my-15 flex justify-center">
+			<div
+				class="relative w-60 sm:w-70 md:w-80 rounded-sm bg-white p-3 pb-5 shadow-md hover:shadow-lg ring-1 ring-black/5 transition-all duration-300 origin-center -rotate-5 hover:-translate-y-5 hover:scale-110 hover:rotate-0">
+				<img
+					src="/selfie.png"
+					alt="Selfie de Remy"
+					class="h-auto w-full object-cover rounded-xs"
+					loading="lazy" />
+				<p
+					class="mt-5 text-[0.68rem] sm:text-xs tracking-widest font-black"
+					style="color: var(--color-burgundy-400)">
+					Selfiiiiiiie !!!
+				</p>
+			</div>
+		</div>
+
 		<p
 			class="italic text-sm sm:text-base"
 			style="color: var(--color-burgundy-500)">
